bc6958b41a768bdd0f15e17142c9fe3a399c0921
GeovaniRustich <geovanirustich2@gmail.com>
2023-05-31 19:56:30 -0300

    Remover Arquivos do Github

--- aula6.py
+++
@@ -1,5 +0,0 @@
-# conversão de tipos, coerção, type conversion, typecasting, coersion
-# é o ato de converter um tipo em outro, tipos imutáveis e primitivos
-# str, int, float, bool
-print ('1' + 1)
-print('a' + 'b')
